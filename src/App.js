bf54b91018a77721a587b5454d55d3c79b062a86
110chinmay <chinmayhendre110@gmail.com>
2024-01-08 17:02:30 +0530

    Updated with router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/loginComponent/Login";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
-  return <div>Hello chinmay</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
