dc8e4fe9fe6ee9984105322114def85a841a3fde
110chinmay <chinmayhendre110@gmail.com>
2024-01-08 21:06:26 +0530

    updated with navbar and doctor form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/loginComponent/Login";
 import Dashboard from "./components/Dashboard/Dashboard";
+import AddDoctorForm from "./components/Forms/AddDoctorForm";
+import AddPatientForm from "./components/Forms/AddPatientForm";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Dashboard />} />
+          <Route path="/add-patient" element={<AddPatientForm />} />
+          <Route path="/add-doctor" element={<AddDoctorForm />} />
+          <Route path="/add-appointment" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </>
